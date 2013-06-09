e58e064640fbcd784c3df54157203b8b0ef8a7dc
woshilapin <woshilapin@gmail.com>
2013-06-09 23:16:14 +0200

    Add help to execute this script
    The script for the configuration of 'woshilapin' is a little tricky to run. Add
    a help message and check if the execution of the script is well before executing
    the modifications.

--- remote_scripts/4-config-woshilapin.sh
+++ remote_scripts/4-config-woshilapin.sh
@@ -9,17 +9,41 @@ source $ROOT_PATH/functions.sh
 
 cd $HOME
 
+ERR_MSG=""
+if [ `whoami` != 'woshilapin' ]
+then
+	print_info "You must be 'woshilapin' user to execute this script"
+	ERR_MSG="You are not 'woshilapin' user."
+fi
 if [ -z $SSH_AGENT_PID ]
 then
 	print_info "You must run a SSH agent for this script."
 	print_info "$> ssh-agent 'bash "$0"'"
-	print_err "No SSH agent found."
+	ERR_MSG="No SSH agent found."
 fi
 if [ \! -f ~/.ssh/woshilapin@gmail.com.rsa ]
 then
 	print_info "You must upload 'woshilapin' SSH keys on 'tuziwo'"
 	print_info "Put 'woshilapin' SSH keys in '/home/woshilapin/.ssh'"
-	print_err "No 'woshilapin' SSH key found."
+	ERR_MSG="No 'woshilapin' SSH key found."
+fi
+if [ -z $ERR_MSG ]
+then
+	print_info ""
+	print_info "To run this script, you should try the following:"
+	print_info "First copy '"$0"' and 'functions.sh' in '/home/woshilapin/'"
+	print_info "Then change owner of these files to 'woshilapin'"
+	print_info "Change user to 'woshilapin'"
+	print_info "Open a shell with an SSH agent"
+	print_info "Finally, run the script"
+	print_info ""
+	print_info "#> cp {"$0",functions.sh} /home/woshilapin/"
+	print_info "#> chown woshilapin:woshilapin /home/woshilapin/*.sh"
+	print_info "#> su woshilapin"
+	print_info "$> ssh-agent 'bash'"
+	print_info "$> bash '"$0"'"
+	print_info ""
+	print_err $ERR_MSG
 fi
 
 ### Add ssh keys
@@ -31,10 +55,13 @@ print_info "Download configuration files"
 git clone git@github.com:woshilapin/dot.git ~/.dot
 cd ~/.dot && git submodule init
 cd ~/.dot && git submodule update
-rm ~/.zshrc
+rm -f ~/.zshrc
 cd ~/.dot && bash update.sh
 cd ~/.dot && echo '[user]' >> .git/config
 cd ~/.dot && echo -e '\tname = "woshilapin"' >> .git/config
 cd ~/.dot && echo -e '\temail = "woshilapin@gmail.com"' >> .git/config
+
+print_info "Change default shell to zsh"
+echo "/bin/zsh" | chsh
 
 exit
